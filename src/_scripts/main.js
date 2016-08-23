d959f3cfd2772854b57a181910f9851463fa27e8
Zan Marolt <marolt.zan@hotmail.com>
2016-08-23 10:33:40 +0200

    Nice transitions, star effect

--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,109 +6,119 @@
 var $ = require('jquery');
 
 var creator = false;
+var xPos = Math.floor(Math.random() * -100) + 0;
+var yPos = Math.floor(Math.random() * -100) + 0;
 
 $(function() {
 
-    var canvas, context, width, height;
-    var particles = [];
+  var canvas, context, width, height;
+  var particles = [];
 
-    function init() {
-      canvas = document.getElementById('main');
-      context = canvas.getContext('2d');
+  function init() {
+    canvas = document.getElementById('main');
+    context = canvas.getContext('2d');
 
-      canvas.width = window.innerWidth;
-      canvas.height = window.innerHeight;
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-      context.globalCompositeOperation = 'lighten';
+    context.globalCompositeOperation = 'lighten';
 
-      drawCanvas();
+    drawCanvas();
 
-      for (var i = 0; i < 50; i++) {
+    for (var i = 0; i < 50; i++) {
+      generateParticle();
+    }
+
+    draw();
+  }
+
+  function drawCanvas() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = 'hsla(199, 54%, 7%, 1)';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+  }
+
+  function draw() {
+
+    drawCanvas();
+
+    for (var i = 0; i < particles.length; i++) {
+
+      if(particles[i].dead)
+        particles.splice(i, 1);
+
+      context.globalCompositeOperation = "lighter";
+      context.beginPath();
+
+      var gradient = context.createRadialGradient(particles[i].x, particles[i].y, 0, particles[i].x, particles[i].y, particles[i].size);
+      gradient.addColorStop(1, 'transparent');
+      gradient.addColorStop(0, particles[i].color);
+
+      context.fillStyle = gradient;
+      context.arc(particles[i].x, particles[i].y, particles[i].size, 0, Math.PI * 2, true);
+      context.closePath();
+      context.fill();
+
+      //Do gravity stuff.
+      particles[i].x += particles[i].Hvelocity;
+      particles[i].y += particles[i].Vvelocity;
+
+      if(particles[i].x - particles[i].size > canvas.width || particles[i].y - particles[i].size > canvas.height)
+        particles[i].dead = true;
+    }
+
+    if (particles.length < 1000)
+      for (var i = 0; i < 10; i++)
         generateParticle();
-      }
 
-      draw();
-    }
+    requestAnimationFrame(draw);
+  }
 
-    function drawCanvas() {
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      context.fillStyle = 'hsla(199, 54%, 7%, 1)';
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    }
+  function generateParticle() {
 
-    function draw() {
+    var colors = [
+      'rgba(41, 114, 34, 0.24)',
+      'rgba(41, 128, 185, 0.3)'
+    ];
 
-      drawCanvas();
+    var color = Math.floor(Math.random() * colors.length) + 0;
 
-      for (var i = 0; i < particles.length; i++) {
+    var particle = {
+      size: Math.floor(Math.random() * 150) + 30,
+      color: colors[color],
+      Hvelocity: Math.floor(Math.random() * 9) + 1,
+      Vvelocity: Math.floor(Math.random() * 9) + 1,
+      dead: false
+    };
 
-        if(particles[i].dead)
-          particles.splice(i, 1);
-
-        context.beginPath();
-
-        var gradient = context.createRadialGradient(particles[i].x, particles[i].y, 0, particles[i].x, particles[i].y, particles[i].size);
-        gradient.addColorStop(0, particles[i].color);
-
-        context.fillStyle = gradient;
-        context.arc(particles[i].x, particles[i].y, particles[i].size, 0, Math.PI * 2, true);
-        context.closePath();
-        context.fill();
-
-        // Move
-        particles[i].x += particles[i].Hvelocity;
-        particles[i].y += particles[i].Vvelocity;
-
-        if(particles[i].x - particles[i].size > canvas.width || particles[i].y - particles[i].size > canvas.height)
-          particles[i].dead = true;
-      }
-
-      if (particles.length < 1000)
-        for (var i = 0; i < 10; i++)
-          generateParticle();
-
-      requestAnimationFrame(draw);
-    }
-
-    function generateParticle() {
-
-      var colors = [
-        'rgba(41, 114, 34, 0.24)',
-        'rgba(41, 128, 185, 0.3)'
-      ];
-
-      var color = Math.floor(Math.random() * colors.length) + 0;
-
-      var particle = {
-        x: Math.floor(Math.random() * -100) + 0,
-        Hvelocity: Math.floor(Math.random() * 12) + 1,
-        Vvelocity: Math.floor(Math.random() * 12) + 1,
-        y: Math.floor(Math.random() * -100) + 0,
-        size: Math.floor(Math.random() * 120) + 30,
-        color: colors[color],
-        dead: false
-      };
-
-      if(creator){
-        if(isNaN(xPos) || isNaN(yPos)){
-          particle.x = Math.floor(Math.random() * -100) + 0;
-          particle.y = Math.floor(Math.random() * -100) + 0;
-        }else{
-
-        }
-
-      }else{
+    if(creator){
+      if(isNaN(xPos) || isNaN(yPos)){
         particle.x = Math.floor(Math.random() * -100) + 0;
         particle.y = Math.floor(Math.random() * -100) + 0;
-
+      }else{
+        particle.x = Math.floor(xPos);
+        particle.y = Math.floor(yPos);
       }
 
-      particles.push(particle);
+
+    }else{
+      particle.x = Math.floor(Math.random() * -100) + 0;
+      particle.y = Math.floor(Math.random() * -100) + 0;
+
     }
 
-    setTimeout(function() {
-      init();
-    }, 300);
+    particles.push(particle);
+  }
+
+  setTimeout(function() {
+    init();
+  }, 500);
+
+});
+
+$("canvas").mousemove(function(event) {
+  xPos = event.clientX;
+  yPos = event.clientY;
 
 });
 
